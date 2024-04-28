59887968a9f0c21541462d43b369a8451ab4e5ce
shikazx <pagcaliwagangerald04@gmail.com>
2024-04-27 21:15:30 -0500

    Set up multiple pages

--- SchoolTools/src/App.jsx
+++ SchoolTools/src/App.jsx
@@ -1,17 +1,30 @@
 import Navbar from './Navbar';
+import Merge from './Merge'
+import Calculator from './Calculator';
 import Home from './Home';
+import { Route, Routes } from 'react-router-dom';
 
 function App() {
-
-
   return (
-    <div className = "Title">
-      <Navbar />
 
-      <div className = "content">
-        <Home />
+      <div>
+        <Navbar />
+        <div className = "content">
+
+      
+
+
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/merge" element={<Merge />} />
+          <Route path="/calculator" element={<Calculator />} />
+        </Routes>
+
+        </div>
+
       </div>
-    </div>
+
+      
 
   );
 }
